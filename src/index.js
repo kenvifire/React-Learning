f2c098224743a0a3c43c49fab012e8d22e15e1a3
kenvi <mrwhite@163.com>
2019-12-15 13:30:11 -0800

    refactor with loop

--- src/index.js
+++ src/index.js
@@ -34,26 +34,24 @@ class Board extends React.Component {
 	}
 
 
+	createSquares() {
+		let rows = []
+		for(let i = 0; i < 3; i++) {
+			let squares = [];
+			for(let j = 0; j < 3; j++) {
+				squares.push(this.renderSquare(3 * i + j));
+			}
+			rows.push(<div className='board-row'>{squares}</div>)
+		}
+		return rows;
+
+	}
 	render() {
 
 		return (
 
 			<div>
-				<div className="board-row">
-					{this.renderSquare(0)}
-					{this.renderSquare(1)}
-					{this.renderSquare(2)}
-				</div>
-				<div className="board-row">
-					{this.renderSquare(3)}
-					{this.renderSquare(4)}
-					{this.renderSquare(5)}
-				</div>
-				<div className="board-row">
-					{this.renderSquare(6)}
-					{this.renderSquare(7)}
-					{this.renderSquare(8)}
-				</div>
+				{this.createSquares()}
 			</div>
 			);
 	}
